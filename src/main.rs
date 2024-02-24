3cd1b2f2b26fefc34007a80203f1b9ae49e5ab6f
taner04 <tanergadomski04@gmail.com>
2024-02-24 22:02:27 +0100

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,45 @@
+use core::str;
 use read_input::prelude::*;
 use std::char;
+use std::io;
 
 fn main() {
-    let mut res = 0;
-    let op;
+    let mut new_run = String::from("y");
 
-    println!("\nEnter your first number");
-    let nr1: i32 = input().get();
+    while new_run.trim() == "y" {
+        let mut res = 0;
+        let op;
 
-    println!("\nEnter your operator(+,-,*,/)");
-    op = input().get();
+        println!("\nEnter your first number");
+        let nr1: i32 = input().get();
 
-    println!("\nEnter your second number");
-    let nr2: i32 = input().get();
+        println!("\nEnter your operator(+,-,*,/)");
+        op = input().get();
 
-    if op == '/' || op == '*' || op == '+' || op == '-' {
-        if op == '/' && nr2 == 0 {
-            println!("\nYou cant divide with 0 or lower");
+        println!("\nEnter your second number");
+        let nr2: i32 = input().get();
+
+        if op == '/' || op == '*' || op == '+' || op == '-' {
+            if op == '/' && nr2 == 0 {
+                println!("\nYou cant divide with 0 or lower");
+            } else {
+                res = calc_result(nr1, nr2, op);
+                println!("\nYour result is: {}", res)
+            }
         } else {
-            res = calc_result(nr1, nr2, op);
-            println!("\nYour result is: {}", res)
+            println!("\nYou Entered an invalid operator");
         }
-    } else {
-        println!("\nYou Entered an invalid operator");
+        println!("\nYou want to make an new calculation? (y/n)");
+        new_run = rerun().to_lowercase();
     }
+}
+
+fn rerun() -> String {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
+    input
 }
 
 fn calc_result(nr1: i32, nr2: i32, op: char) -> i32 {
@@ -32,11 +48,7 @@ fn calc_result(nr1: i32, nr2: i32, op: char) -> i32 {
         '+' => res = nr1 + nr2,
         '-' => res = nr1 - nr2,
         '*' => res = nr1 * nr2,
-        '/' => {
-            if nr2 > 0 {
-                res = nr1 / nr2;
-            }
-        }
+        '/' => res = nr1 / nr2,
         _ => {
             println!("\nYou entered an invalid operator");
         }
